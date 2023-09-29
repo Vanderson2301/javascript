0cf832bc9b5542f10080ed332e4f30d973f6d99b
Vanderson Caetano <111813062+Vanderson2301@users.noreply.github.com>
2023-09-29 19:31:37 -0300

    condição composta

---
+++ aula11/ex009.js
@@ -0,0 +1,17 @@
+var vel = "40 km/h"
+console.log(`Meu carro está rodando a ${vel}`)
+if (vel > "60") {
+    console.log("Você está multado, seu palhaço!")
+}
+
+/* Chamamos o "if" de "Se por acaso"
+    Isso é uma condição simples
+*/
+
+var pneu = "3"
+    console.log(`Fui no mecânico fazer uma revisão, lá a maquina constatou que havia ${pneu} pneus calibrados, então ela avisou:`)
+if (vel < "4"){
+    console.log ("ATENÇÃO! Você está com algum pneu descalibrado.")}
+ else {
+    console.log("Você está com todos os pneus calibrados, siga seu caminho, palhaço.")
+}
